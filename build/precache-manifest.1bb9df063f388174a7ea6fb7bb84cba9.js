7ce9dcb61f1fa5b8146164f419ad51fa8dee8e0d
LeonMilosevic <leonleon787@yahoo.com>
2020-09-11 15:07:42 +0300

    testing

---
+++ build/precache-manifest.1bb9df063f388174a7ea6fb7bb84cba9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca86ac35f70efe9ef94625ae8222b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dbcbfcca7b0ed18b4a3",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "5803d89cffaf7199448d",
+    "url": "/static/css/main.28f29aa5.chunk.css"
+  },
+  {
+    "revision": "7dbcbfcca7b0ed18b4a3",
+    "url": "/static/js/2.4d11f300.chunk.js"
+  },
+  {
+    "revision": "c48c2a5ab75d35dc8f8136d8d35ba197",
+    "url": "/static/js/2.4d11f300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5803d89cffaf7199448d",
+    "url": "/static/js/main.c111cf85.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e9c7df9b1e26ea50516963a010f15931",
+    "url": "/static/media/Logo.e9c7df9b.svg"
+  }
+]);
